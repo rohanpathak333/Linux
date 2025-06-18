e277317683b656df76398bc02278f618c3f8e490
rohanpathak333 <rohanpathak333@gmail.com>
2025-06-18 13:25:22 +0000

    Linux variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="AWS"
+environment="dev"
+echo "My cloud is $name, running on $environment"
+echo "-----------welcome to $name--------------"
+echo "We are using $SHELL"
+echo "-----------------"
+echo "we are at $HOME and today is $(date)"
